a828e128857939eacf5ef48af6f211a4dd7ea67f
xiaozhang <xiaozhang@123.com>
2021-10-25 14:59:29 +0800

    use magicmind model to build mm model

--- demo/MagicMind/python/mm_infer.py
+++ demo/MagicMind/python/mm_infer.py
@@ -5,6 +5,8 @@ import argparse
 import sys
 from demo.MagicMind.python.magicmind_model import MagicMindModel
 
+import torch
+
 import magicmind.python.runtime as mm
 from magicmind.python.runtime import Context
 
@@ -15,39 +17,15 @@ from yolox.data.data_augment import preproc as preprocess
 from yolox.data.datasets import COCO_CLASSES
 from yolox.utils import mkdir, multiclass_nms, demo_postprocess, vis
 
-# 生成模型
-def build_model(args):
-    model = mm.Model()
-    model.deserialize_from_file(args.mm_file_name)
-    assert model != None, "Failed to build model"
-    return model
-
-# 创建推理模型所需的上下文
-def create_context(model, args):
-    econfig = mm.Model.EngineConfig()
-    econfig.device_type = "MLU"
-    engine = model.create_i_engine(econfig)
-    assert engine != None, "Failed to create engine"
-    context = engine.create_i_context()
-    
-    if args.mm_dump:
-        dumpinfo = Context.ContextDumpInfo(path="/tmp/output_pb/", tensor_name=[], dump_mode=1, file_format=0)
-        # dumpinfo.val.dump_mode = 1   # -1 关闭dump 模式; 0 dump 指定tensor; 1 dump所有tensor; 2 dump 输出tensor
-        # dumpinfo.val.path = "/tmp/output"# 将dump 结果存放到/tmp/output ⽬录下
-        # dumpinfo.val.tensor_name = [] # dump 所有tensor 信息
-        # dumpinfo.val.file_format = 0 # 0 ⽂件保存为pb; 1 ⽂件保存为pbtxt
-        context.set_context_dump_info(dumpinfo)
-
-    return context
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
     # 基础参数，必选
     parser.add_argument("--builder_config", help="",type=str, default="builder_config.json")
-    parser.add_argument("--mm_file_name", help="", type=str, default="yolox_m_quantized.model")
+    parser.add_argument("--mm_file_name", help="", type=str, default="yolox_m_int8fp16.model")
     parser.add_argument("--mm_dump", help="", action="store_true")
     parser.add_argument("--image_path", help="", type=str, default="../../../assets/dog.jpg")
+    parser.add_argument("--out_path", help="", type=str, default="mm_out.jpg")
     parser.add_argument("--input_shapes", help="", type=list, default=[[1, 3, 640, 640]])
     parser.add_argument("--input_dtypes", help="", type=list, default=["float32"])
     parser.add_argument("--with_p6", action="store_true", help="")
@@ -60,9 +38,11 @@ if __name__ == '__main__':
     origin_img = cv2.imread(args.image_path)
     img, ratio = preprocess(origin_img, input_shape)
 
-    outputs = mm_model(img[np.newaxis, ])
+    img = img[np.newaxis, ]
+    # img = torch.from_numpy(img)
+    outputs = mm_model(img)
 
-    predictions = demo_postprocess(outputs[0].asnumpy(), input_shape, p6=args.with_p6)[0]
+    predictions = demo_postprocess(outputs.numpy(), input_shape, p6=args.with_p6)[0]
 
     boxes = predictions[:, :4]
     scores = predictions[:, 4:5] * predictions[:, 5:]
@@ -79,4 +59,4 @@ if __name__ == '__main__':
         origin_img = vis(origin_img, final_boxes, final_scores, final_cls_inds,
                          conf=0.3, class_names=COCO_CLASSES)
 
-    cv2.imwrite("mm_out.jpg", origin_img)
+    cv2.imwrite(args.out_path, origin_img)
