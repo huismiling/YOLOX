7861aae872c1f96c9cbb0507918ce427d83553b1
xiaozhang <xiaozhang@deepglint.com>
2021-10-28 10:18:11 +0800

    use cpu to evaluate.

--- yolox/evaluators/coco_evaluator.py
+++ yolox/evaluators/coco_evaluator.py
@@ -72,7 +72,7 @@ class COCOEvaluator:
             summary (sr): summary info of evaluation.
         """
         # TODO half to amp_test
-        tensor_type = torch.cuda.HalfTensor if half else torch.cuda.FloatTensor
+        tensor_type = torch.FloatTensor  # torch.cuda.HalfTensor if half else torch.cuda.FloatTensor
         model = model.eval()
         if half:
             model = model.half()
@@ -122,7 +122,7 @@ class COCOEvaluator:
 
             data_list.extend(self.convert_to_coco_format(outputs, info_imgs, ids))
 
-        statistics = torch.cuda.FloatTensor([inference_time, nms_time, n_samples])
+        statistics = torch.Tensor([inference_time, nms_time, n_samples]).type(tensor_type)
         if distributed:
             data_list = gather(data_list, dst=0)
             data_list = list(itertools.chain(*data_list))
